d15faa9dffbd2ebd5c45df3d1de3e1a37fcf1f83
mahadev <mahadev@DESKTOP-192B6UQ>
2025-07-15 11:26:43 +0530

    commit

--- JaggedArray.cs
+++ JaggedArray.cs
@@ -11,12 +11,15 @@ namespace net_nvb
         public static void Main(string[] args)
         {
             // Declare a jagged array with 3 rows
-            int[][] jaggedArray = new int[4][];
+            int[][] jaggedArray = new int[7][];
             // Initialize each row with different lengths
             jaggedArray[0] = new int[1]; // First row has 2 elements
             jaggedArray[1] = new int[2]; // Second row has 3 elements
             jaggedArray[2] = new int[3]; // Third row has 4 elements
             jaggedArray[3] = new int[4]; // Fourth row has 5 elements
+            jaggedArray[4] = new int[3]; // Fifth row has 6 elements
+            jaggedArray[5] = new int[2]; // Sixth row has 7 elements
+            jaggedArray[6] = new int[1]; // Seventh row has 8 elements
             // Fill the jagged array with values
             for (int i = 0; i < jaggedArray.Length; i++)
             {
