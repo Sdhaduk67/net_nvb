10e6dcad67f93884fe6f7582c0a50e91412f1b36
mahadev <mahadev@DESKTOP-192B6UQ>
2025-07-15 11:20:49 +0530

    commit

---
+++ 2array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_nvb
+{
+    internal class _2array
+    {
+        public static void Main(string[] args)
+        {
+            int[] ary1 = new int[1];
+            int[] ary2 = new int[1];
+            int[] sumAry = new int[1];
+            Console.WriteLine("Enter elements for first array:");
+            for (int i = 0; i < ary1.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                ary1[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Enter elements for second array:");
+            for (int i = 0; i < ary2.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                ary2[i] = Int32.Parse(Console.ReadLine());
+            }
+            // Calculate the sum of the two arrays
+            for (int i = 0; i < sumAry.Length; i++)
+            {
+                sumAry[i] = ary1[i] + ary2[i];
+            }
+            // Display the result
+            Console.WriteLine("Sum of the two arrays:");
+            foreach (var item in sumAry)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ JaggedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_nvb
+{
+    internal class JaggedArray
+    {
+        public static void Main(string[] args)
+        {
+            // Declare a jagged array with 3 rows
+            int[][] jaggedArray = new int[4][];
+            // Initialize each row with different lengths
+            jaggedArray[0] = new int[1]; // First row has 2 elements
+            jaggedArray[1] = new int[2]; // Second row has 3 elements
+            jaggedArray[2] = new int[3]; // Third row has 4 elements
+            jaggedArray[3] = new int[4]; // Fourth row has 5 elements
+            // Fill the jagged array with values
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    //Console.Write($"Enter element at row {i}, column {j}: ");
+                    jaggedArray[i][j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+            // Display the jagged array
+            Console.WriteLine("Jagged Array Elements is:");
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
